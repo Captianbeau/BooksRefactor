df70f6c909e7945b909c847c08db822a44b0e7be
Capti <captianbeau@gmail.com>
2024-04-05 15:29:59 -0600

    added apollo to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,19 @@ import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client'
 
 import Navbar from './components/Navbar';
 
+const client = new ApolloClient({
+  uri:'/graphql',
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client = {client}>
     <>
       <Navbar />
       <Outlet />
     </>
+    </ApolloProvider>
   );
 }
 
