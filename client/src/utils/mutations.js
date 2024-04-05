383bfda036cea0b3a657081ddf26ed7a78087c63
Capti <captianbeau@gmail.com>
2024-04-05 14:07:47 -0600

    started mutations on client side.

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,13 @@
+import{gql} from '@apollo/client';
+
+export const ADD_USER = gql `
+mutation createUser($username:String, $email: String!, $password: String!){
+    createUser(username: $username, email: $email, password: $password){
+        _id
+        username
+        email
+        password
+        savedBooks
+    }
+}
+`;
