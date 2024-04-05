4249980476d79e44fce484d86926b800d95e7dc6
Capti <captianbeau@gmail.com>
2024-04-05 14:14:24 -0600

    added login to the mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -3,11 +3,19 @@ import{gql} from '@apollo/client';
 export const ADD_USER = gql `
 mutation createUser($username:String, $email: String!, $password: String!){
     createUser(username: $username, email: $email, password: $password){
-        _id
-        username
-        email
-        password
-        savedBooks
-    }
+        token
+        user {
+          _id
+        }
 }
+`;
+export const LOGIN = gql`
+  mutation login($username: String!, $email: String!, $password: String!) {
+    login(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
 `;
